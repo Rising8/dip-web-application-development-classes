3ab36f55a441797f2386e8096f69eeca66d9acb9
Edmand <edmand66@hotmail.com>
2025-08-04 11:32:50 +0800

    Session 2 code

--- session-02/assets/js_starter/scripts_challenge_3.js
+++ session-02/assets/js_starter/scripts_challenge_3.js
@@ -1,12 +1,18 @@
 // Challenge 3 Starter
 // Initialise the array
+let numbers = [1,2,3,4,5];
 
 // Check if the array includes the number 5
+if (numbers.includes(5)) {
+    // If true, filter out numbers less than 5 
+    let filtered = numbers.filter(num => num >= 5)
 
-// If true, filter out numbers less than 5
+    // Square each number in the resulting array 
+    let squared = filtered.map(num => num * num);
 
-// Then square each number in the resulting array
-
-// Output the final array
-
-// Output a message if the number 5 is not in the array
+    // Output the final array 
+    console.log("Squared numbers:", squared);
+} else {
+    // Output a message if the number 5 is not in the array
+    console.log("The number 5 is not in the array.");
+}
